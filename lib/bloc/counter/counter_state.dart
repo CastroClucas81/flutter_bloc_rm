c14be35256703e9087396c48b0225eea188aabdb
Lucas Cardoso de Castro <sky.lucas2013@gmail.com>
2022-03-06 17:44:54 -0300

    put equatable in classes

--- lib/bloc/counter/counter_state.dart
+++ lib/bloc/counter/counter_state.dart
@@ -1,5 +1,10 @@
-class CounterState {
+import 'package:equatable/equatable.dart';
+
+class CounterState extends Equatable {
   final int counter;
 
   CounterState({required this.counter});
+
+  @override
+  List<Object?> get props => [counter];
 }
--- lib/bloc/navigator/navigator_state.dart
+++ lib/bloc/navigator/navigator_state.dart
@@ -1,11 +1,17 @@
+import 'package:equatable/equatable.dart';
+
 enum EnumPages {
   counterPage,
   homePage,
   testePage,
 }
 
-class NavigatorState {
+class NavigatorState extends Equatable {
   final EnumPages page;
 
   NavigatorState({required this.page});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [page];
 }
